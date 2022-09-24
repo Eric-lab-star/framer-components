f0cb253dfa4eb0c55aab7afdeb1ed041522f759d
eric-lab-star <cyon256@icloud.com>
2022-09-24 20:59:16 +0900

    simple slider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex w-screen h-screen justify-center items-center ">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
