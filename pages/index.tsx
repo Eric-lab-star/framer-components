c5d23bee361ac20476bd55dbd14116ac612bca7e
eric-lab-star <cyon256@icloud.com>
2022-10-04 22:16:30 +0900

    left

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,28 @@ import type { NextPage } from "next";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
+const variant = {
+  Left: {
+    x: "-100vw",
+  },
+  Right: {
+    x: "100vw",
+  },
+};
+
 const Home: NextPage = () => {
   const db = [1, 2, 3, 4, 5];
   const [number, setNumber] = useState(0);
+  const [isLeft, setIsLeft] = useState(false);
   const moveLeft = () => {
+    setIsLeft(true);
     setNumber((prev) => (prev = prev + 1));
   };
+  const moveRight = () => {
+    setIsLeft(false);
+    setNumber((prev) => (prev = prev - 1));
+  };
+
   return (
     <div>
       <div className="relative">
@@ -15,9 +31,10 @@ const Home: NextPage = () => {
           <motion.div
             key={number}
             className=" bg-blue-400 absolute top-10 h-40 aspect-square flex justify-center items-center text-white text-xl font-bold"
-            initial={{ x: "100vw" }}
+            initial={isLeft ? "Right" : "Left"}
             animate={{ x: "0vw" }}
-            exit={{ x: "-100vw" }}
+            exit={isLeft ? "Left" : "Right"}
+            variants={variant}
             transition={{ duration: 1 }}
           >
             {number}
@@ -30,7 +47,10 @@ const Home: NextPage = () => {
       >
         left
       </button>
-      <button className="px-2 shadow-md  bg-amber-400 m-2 rounded-md text-white">
+      <button
+        onClick={moveRight}
+        className="px-2 shadow-md  bg-amber-400 m-2 rounded-md text-white"
+      >
         right
       </button>
     </div>
